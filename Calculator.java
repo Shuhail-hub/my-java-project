2274b8bd84db957df4b0be03788deecbf0dbe602
Shuhail-hub <shuhailshafeek@gmail.com>
2025-03-06 17:19:30 +0530

    Add simple Java calculator addition coden program and README

---
+++ Calculator.java
@@ -0,0 +1,12 @@
+// Addition.java
+public class Addition {
+    public static void main(String[] args) {
+        // Add two numbers
+        int num1 = 3;
+        int num2 = 5;
+        int sum = num1 + num2;
+
+        // Print the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple Java Addition Program
+
+This project contains a simple Java program that defines a class `Calculator`. The class performs an addition of two numbers and prints the result.
