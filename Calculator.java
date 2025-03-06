0b7525619c1dae605a9508091615989e717b925a
Shuhail-hub <shuhailshafeek@gmail.com>
2025-03-06 17:28:48 +0530

    variable change

--- Calculator.java
+++ Calculator.java
@@ -3,7 +3,7 @@ public class Addition {
     public static void main(String[] args) {
         // Add two numbers
         int num1 = 3;
-        int num2 = 5;
+        int num2 = 15;
         int sum = num1 + num2;
 
         // Print the result
